f53493b8744a9ac91fcee0eebfa53f9e9e3caffd
asutosh-147 <behuriaasutosh7@gmail.com>
2025-03-29 00:02:28 +0530

    image fix

--- src/Gift.tsx
+++ src/Gift.tsx
@@ -23,8 +23,8 @@ const Gift = () => {
         <div className='flex flex-col items-center justify-center gap-10 h-screen bg-gray-600'>
             {showConfetti && <MyConfetti />}
             <div className='text-white font-medium text-3xl tracking-widest'>Remember?</div>
-            <img src="loid_forger.jpg" alt="gift" height={1920/1.5} width={1080/1.5} className='rounded-xl shadow-2xl hover:scale-110 duration-300' />
-            <a href="loid_forger.jpg" download={"gift.jpg"} className='text-white font-semibold capitalize bg-gray-900 py-3 px-5 rounded-lg active:scale-90 duration-200'>download</a>
+            <img src="/loid_forger.jpg" alt="gift" height={1920/1.5} width={1080/1.5} className='rounded-xl shadow-2xl hover:scale-110 duration-300' />
+            <a href="/loid_forger.jpg" download={"gift.jpg"} className='text-white font-semibold capitalize bg-gray-900 py-3 px-5 rounded-lg active:scale-90 duration-200'>download</a>
         </div>
     )
 }
